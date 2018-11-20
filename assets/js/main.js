a590bf6e9bbc669e4ce63eaabe77f5b8cd8e5a06
AsunderAshi <shishkinanton14@gmail.com>
2018-11-21 01:07:40 +0500

    NEw commit

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+function openLink(evt, linkName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("myLink");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" ruta-button-bg", "");
+    }
+    document.getElementById(linkName).style.display = "block";
+    evt.currentTarget.className += " ruta-button-bg";
+  }
+  function ToggleMenu() {
+    var element = document.getElementById("sidemenu"),
+        backdrop = document.getElementById("backdrop")
+        element.classList.toggle('toggled');
+        backdrop.classList.toggle('toggled')
+   
+  }
+  function ToggleAccordeon (element) {
+      element.classList.toggle('toggled');
+      element.nextElementSibling.classList.toggle('toggled')
+  }
+  document.getElementsByClassName("tablink")[0].click();
