5d7f3e028dd4ed181edab452daa5ecd477a03dbf
AsunderAshi <shishkinanton14@gmail.com>
2018-11-21 01:13:24 +0500

    Fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,16 +10,19 @@ function openLink(evt, linkName) {
     }
     document.getElementById(linkName).style.display = "block";
     evt.currentTarget.className += " ruta-button-bg";
-  }
-  function ToggleMenu() {
+}
+
+function ToggleMenu() {
     var element = document.getElementById("sidemenu"),
         backdrop = document.getElementById("backdrop")
-        element.classList.toggle('toggled');
-        backdrop.classList.toggle('toggled')
-   
-  }
-  function ToggleAccordeon (element) {
-      element.classList.toggle('toggled');
-      element.nextElementSibling.classList.toggle('toggled')
-  }
-  document.getElementsByClassName("tablink")[0].click();
+    element.classList.toggle('toggled');
+    backdrop.classList.toggle('toggled')
+
+}
+
+function ToggleAccordeon(element) {
+    element.classList.toggle('toggled');
+    element.nextElementSibling.classList.toggle('toggled')
+}
+
+document.getElementsByClassName("tablink")[0].click();
