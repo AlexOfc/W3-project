b20e82fa3297eab2da5609ca9eb11f5b56f1c49b
AlexOfc <dekoverover@gmail.com>
2018-11-21 17:10:17 +0500

    js fxes

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,6 @@ function openLink(evt, linkName) {
     document.getElementById(linkName).style.display = "block";
     evt.currentTarget.className += " ruta-button-bg";
     
-    imageHeight()
 }
 
 function ToggleMenu() {
@@ -29,33 +28,6 @@ function ToggleAccordeon(element) {
     element.nextElementSibling.classList.toggle('toggled')
 }
 
-/*function imageHeight() {
-    var element = document.getElementById("selection-img"),
-        secWindow = document.getElementById("section-window"),
-        defaultHeight = document.getElementById("selection-img").naturalHeight,
-        tablinks = document.getElementsByClassName("tablink"),
-        x = document.getElementsByClassName("myLink");
-    
-    var y = document.getElementsByClassName('ruta-button-bg');
-        var aNode = y[0];
-        if (window.innerWidth > 650){
-            if ((secWindow.offsetHeight > element.height) && !aNode.classList.contains('clicked')) {
-                element.height = secWindow.offsetHeight * 1.1;
-                aNode.classList.add('clicked')
-            } else if (!aNode.classList.contains('clicked')){
-                for (i = 0; i < x.length; i++) {
-                    tablinks[i].className = tablinks[i].className.replace(" clicked", "");
-                }
-                element.height = defaultHeight;
-            } 
-        } else {
-            element.height = secWindow.offsetHeight * 1.1;
-        }
-             
-    
-    
-}*/
 document.body.onload = function() {    
-    //imageHeight();
     document.getElementsByClassName("tablink")[0].click();
 }
